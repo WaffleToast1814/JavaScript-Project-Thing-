4045170af080bb3f85beba66f432fe2caa0805c7
WaffleToast <148481626+WaffleToast1814@users.noreply.github.com>
2024-03-28 10:03:57 -0700

    Delete JavaScript Project directory

--- JavaScript Project/app3.js
+++
@@ -1,8 +0,0 @@
-let button = document.querySelector('.Click-Button-3');
-let audio = document.getElementById('KuruKuru-2');
-
-function play(){
-audio.play()
-}
-
-button.addEventListener('click', getAudio)
